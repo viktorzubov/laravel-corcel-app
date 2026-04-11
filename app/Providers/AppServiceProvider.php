61f8291f30c070b58bb1807ac685b211da53ea98
Viktor Zubov <13069159+viktorzubov@users.noreply.github.com>
2026-04-11 10:49:31 -0400

    refactor: remove PostObserver as WordPress bypasses Eloquent events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Post;
-use App\Observers\PostObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Post::observe(PostObserver::class);
+        //
     }
 }
